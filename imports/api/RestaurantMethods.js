bb7a99781c969a3b06f7b3a5c1359650180b3e9d
Hisham <ranjithp2294@gmail.com>
2024-10-07 07:46:34 +0530

    bug in updation userId has been resolved

--- imports/api/RestaurantMethods.js
+++ imports/api/RestaurantMethods.js
@@ -2,7 +2,8 @@ import {Meteor} from 'meteor/meteor'
 import { RestaurantCollection } from './RestaurantCollection'
 
 Meteor.methods({
-    "bookSeat":({_id,tableName,seat})=>{
-        RestaurantCollection.updateAsync({_id,'tables.tableName':tableName,'tables.seats.seat':seat},{$set:{'tables.$[tableName].seats.$[seatNo].status':"booked"}},{arrayFilters:[{'tableName.tableName':tableName},{'seatNo.seat':seat}]})
+    "bookSeat":function({_id,tableName,seat}){
+        console.log("this.userId",this.userId)
+        RestaurantCollection.updateAsync({_id,'tables.tableName':tableName,'tables.seats.seat':seat},{$set:{'tables.$[tableName].seats.$[seatNo].status':"booked","tables.$[tableName].seats.$[seatNo].userId":this.userId}},{arrayFilters:[{'tableName.tableName':tableName},{'seatNo.seat':seat}]})
     },
 })
